bf54cbd7ac81310a1f8cfa0d16a39355a61c29a1
Abel <abelcg86@gmail.com>
2017-10-09 20:30:45 -0300

    commit inicial

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace proyecto1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = "Nicole";
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola Mundo!");
+            Console.WriteLine(nombre);
+            Console.WriteLine("hola "+ nombre);
+            Console.WriteLine("cantidad de parametros: "+ args.Length);
+            int n = args.Length;
+           /*switch (n)
+            {
+                caso 0:
+                    Console.WriteLine("no tiene parametros");
+                    break;
+                case 1:
+                    Console.WriteLine("tiene un parametro");
+                    break;
+                default:
+                    Console.WriteLine("tiene "+ n + " parametros");
+                    break;
+            }*/
+        }
+    }
+}
