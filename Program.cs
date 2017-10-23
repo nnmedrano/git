1ee4396d4611344778c0f99b37a66525f45fec90
Abel <abelcg86@gmail.com>
2017-10-23 18:19:57 -0300

    segunda vuelta

--- Program.cs
+++ Program.cs
@@ -7,24 +7,58 @@ namespace proyecto1
         static void Main(string[] args)
         {
             string nombre = "Nicole";
+            int edad = 0;
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hola Mundo!");
-            Console.WriteLine(nombre);
-            Console.WriteLine("hola "+ nombre);
-            Console.WriteLine("cantidad de parametros: "+ args.Length);
-            int n = args.Length;
-           /*switch (n)
+            Console.WriteLine("Cual es tu nombre?");
+            nombre = Console.ReadLine();
+            
+
+            while(!nombre.Equals("bailar"))
+            {   
+            Console.WriteLine("Cual es tu edad?");
+            int.TryParse(Console.ReadLine(), out edad);
+            }
+
+            if(edad >= 21)
             {
-                caso 0:
-                    Console.WriteLine("no tiene parametros");
-                    break;
-                case 1:
-                    Console.WriteLine("tiene un parametro");
-                    break;
-                default:
-                    Console.WriteLine("tiene "+ n + " parametros");
-                    break;
-            }*/
+                Console.WriteLine("Es mayor de edad");
+            }
+            else
+            {
+                Console.WriteLine("Es menor");
+            }
+
+            Console.WriteLine("Cual es tu nombre?");
+            nombre = Console.ReadLine();
+
+            for(int i=0;i<5;i++)
+            {
+                Console.WriteLine("chau");
+            }
+
+            int j=0;
+            while(j<5)
+            {
+                Console.WriteLine("Chau a todos");
+            }
+
+            string color = "magenta";
+            string[] colores = new string[]{ "azul", color, "blanco", "negro","amarillo", "magenta"};
+            string[] coloresotro = new string[3];
+            Console.WriteLine("el tercer color es el:" + colores[2]);
+            colores[0] = "verde";
+
+            for(int k=0; k < colores.Length ; k++)
+            {
+                Console.WriteLine("Color:" + colores[k]);
+            }
+
+            foreach (string item in colores)
+            {
+                Console.WriteLine("Color:" + item);
+            }
+            
         }
     }
 }
